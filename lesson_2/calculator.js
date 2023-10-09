24d8f084dfe35eba7384a3bf196987178df7c316
larebear007 <larencozart@gmail.com>
2023-10-09 11:08:00 -0500

    Add Bonus Feature: internationalization, divide by zero

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,46 +1,58 @@
 const readline = require('readline-sync');
 const msg = require('./calculator_messages.json');
+const userLanguage = getLanguage();
 
 function prompt(message) {
   console.log(`=> ${message}`);
+}
+function getLanguage () {
+  prompt(msg.en.language);
+  prompt(msg.es.language);
+  prompt(msg.fr.language);
+  let language = readline.question();
+
+  while (!['en', 'es', 'fr'].includes(language)) {
+    prompt(msg.en.invalidLanguage);
+    prompt(msg.es.invalidLanguage);
+    prompt(msg.fr.invalidLanguage);
+    language = readline.question();
+  }
+  return language;
 }
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
 let getNumber1 = () => {
-  prompt(msg.num1);
+  prompt(msg[userLanguage]['num1']);
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(msg.invalidNum);
+    prompt(msg[userLanguage]['invalidNum']);
     number1 = readline.question();
   }
   return number1;
 };
-
 let getNumber2 = () => {
-  prompt(msg.num2);
+  prompt(msg[userLanguage]['num2']);
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(msg.invalidNum);
+    prompt(msg[userLanguage]['invalidNum']);
     number2 = readline.question();
   }
   return number2;
 };
-
 let getOperation = () => {
-  prompt(msg.operation);
+  prompt(msg[userLanguage]['operation']);
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(msg.invalidOperation);
+    prompt(msg[userLanguage]['invalidOperation']);
     operation = readline.question();
   }
   return operation;
 };
-
 let calculationResult = (number1, number2, operation) => {
   let output;
   switch (operation) {
@@ -59,21 +71,32 @@ let calculationResult = (number1, number2, operation) => {
   }
   return output;
 };
+function displayResult () {
+  let userNumber1 = getNumber1();
+  let userNumber2 = getNumber2();
+  let userOperation = getOperation();
+  while (userNumber2 === '0' && userOperation === '4') {
+    prompt(msg[userLanguage]['divideByZero']);
+    userOperation = getOperation();
+  }
+
+  prompt(`${msg[userLanguage]['result']} ${calculationResult(userNumber1, userNumber2, userOperation)}`);
+}
 
 function displayCalculator () {
   while (true) {
-    prompt(msg.welcome);
-    prompt(`The result is: ${calculationResult(getNumber1(), getNumber2(), getOperation())}`);
+    prompt(msg[userLanguage]['welcome']);
+    displayResult();
 
-    prompt(msg.playAgain);
+    prompt(msg[userLanguage]['playAgain']);
     let response = readline.question();
 
-    while (!['y', 'n'].includes(response)) {
-      prompt(msg.invalidPlayAgain);
+    while (!['1', '0'].includes(response)) {
+      prompt(msg[userLanguage]['invalidPlayAgain']);
       response = readline.question();
     }
-    if (response === 'n') {
-      prompt(msg.goodbye);
+    if (response === '0') {
+      prompt(msg[userLanguage]['goodbye']);
       break;
     } else {
       continue;
