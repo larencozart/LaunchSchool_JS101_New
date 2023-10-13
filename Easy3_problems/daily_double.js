2806d5711870f2219feea60ed69952cdc6542af1
larebear007 <larencozart@gmail.com>
2023-10-12 20:16:38 -0500

    Delete test code

--- Easy3_problems/daily_double.js
+++ Easy3_problems/daily_double.js
@@ -5,16 +5,14 @@ original string with all consecutive duplicate characters
 collapsed into a single character.
 */
 
-// function crunch (str) {
-//   let condensedStr = '';
-//   for (let idx = 0; idx < str.length; idx += 1) {
-//     if (str[idx] === str[idx - 1]) continue;
-//     condensedStr += str[idx];
-//   }
-//   return condensedStr;
-// }
-
-const crunch = str => str.split('').map(i => i).join('');
+function crunch (str) {
+  let condensedStr = '';
+  for (let idx = 0; idx < str.length; idx += 1) {
+    if (str[idx] === str[idx - 1]) continue;
+    condensedStr += str[idx];
+  }
+  return condensedStr;
+}
 
 console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
 crunch('4444abcabccba');              // "4abcabcba"
